3276381d323122c3375e3fed32b7829593698f6f
Nikhil Kumar <nk90600@gmail.com>
2019-10-15 11:47:58 +0530

    redis updated

--- services/redis.py
+++ services/redis.py
@@ -3,13 +3,13 @@ from fundoo.settings import DB, PORT
 red = redis.StrictRedis(host="localhost", db=DB, port=PORT)
 
 
-class Redis:
+# class Redis:
 
-    def set(self, key, value):
-        red.set(key, value)
+#     def set(self, key, value):
+#         red.set(key, value)
 
-    def get(self, key):
-        red.get(key)
+#     def get(self, key):
+#         red.get(key)
 
-    def delete(self, key):
-        red.delete(key)
+#     def delete(self, key):
+#         red.delete(key)
